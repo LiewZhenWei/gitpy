327c67f058199d5da34e576cfdcadabf929214d9
LiewZhenWei <zhenwei3557@g.ncu.edu.tw>
2025-03-12 17:59:18 +0800

    py

--- test.py
+++ test.py
@@ -7,3 +7,8 @@ print(fruit_is.upper())
 print('I '+'python '+'love.')
 print('i'*10)
 print('I {} python {}'.format('love','.'))
+print('{0:8} | {1:>8}'.format('Fruit',"Quantity"))
+print('{0:8} | {1:>8}'.format('Fruit',"123"))
+#输入
+fruit=input('Enter your favorite fruit: ')
+print('{} is your favorite fruit'.format(fruit))
